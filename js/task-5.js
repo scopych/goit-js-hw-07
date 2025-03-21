e3b3afc4707881cab489c220b45c3a9ecf9a157b
scopych <scopych@gmail.com>
2025-03-21 18:53:03 +0200

    Complete hw 7.

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,19 @@
+'use strict';
+
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+
+btnEl.addEventListener('click', changeColor);
+
+function changeColor(event) {
+  const colorToChange = getRandomHexColor();
+
+  event.preventDefault();
+  bodyEl.style.backgroundColor = colorToChange;
+  spanEl.textContent = colorToChange;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
